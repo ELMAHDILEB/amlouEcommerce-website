0a308f83885c3ce462627dafb623bc6b4dfe5a39
mrpoa <mrpoa@pop-os.localdomain>
2025-04-13 07:02:14 +0100

    remove height and add height fix in article tag

--- src/components/FeaturedProducts.jsx
+++ src/components/FeaturedProducts.jsx
@@ -6,12 +6,12 @@ const FeaturedProducts = ()=> {
     <section className="w-full flex flex-col items-center justify-center gap-10 p-4 mx-auto my-0 font-montserrat">
       <h1 className="break-normal uppercase tracking-[4px] font-black text-[var(--primary)] leading-[32px]" style={{fontSize: "clamp(20px, 4vw, 50px)"}}>Featured Products</h1>
       <section className="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 palce-items-center md:place-items-stretch gap-4">
-        <article className="h-[calc(350px/2)] flex flex-col items-center bg-white cursor-pointer">
-          <div className="w-fit h-full">
+        <article className="h-[350px] flex flex-col items-center bg-white cursor-pointer">
+          <div className="w-fit h-[200px]">
             <img className="w-full h-full object-cover" src={noixcajou} alt="noix de cajou" />
           </div>
 
-          <div className="w-full h-full flex flex-col items-center justify-between gap-4">
+          <div className="w-full h-[100px] flex flex-col items-center justify-between gap-4">
             <h2 className="text-[15px] font-normal">Title Product</h2>
             <span className="">85 DHS</span>
             <div className="flex items-center gap-2">
