3bab60747934d66c4a10c39b96dceb9f4b7ccd2f
ELMAHDILEB <elmahdibelcadi@gmail.com>
2025-04-19 21:41:09 +0000

    add grid system to section footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,36 @@
 import React from 'react'
 
-const Footer = ()=> {
-  return (
-    <footer>
-      <h1>Footer</h1>
+const Footer = () => {
+    return (
+        <footer>
 
-      <p>&copy; {new Date().getFullYear()} Amlou Inou. All rights reserved</p>
-    </footer>
-  )
+            <section className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center'>
+                <div className=''>
+                    <h1>Amlou Inou</h1>
+                    <p>your destination for organic & naturel food</p>
+                </div>
+              
+
+              <div>
+              <div className='flex items-center gap-5'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
+                        </svg>
+                        <span>+212 6XX-XXXXXX</span>
+                    </div>
+                    <div className='flex items-center gap-5'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                        </svg>
+
+                        <span>info@amlouind.com</span>
+                    </div>
+              </div>
+            </section>
+
+            <p>&copy; {new Date().getFullYear()} Amlou Inou. All rights reserved</p>
+        </footer>
+    )
 }
 
 export default Footer;
